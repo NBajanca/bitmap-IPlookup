c2c9fa1c477a6317f6d8a0d977f2df1821470e50
Nuno Bajanca <nunombajanca@gmail.com>
2016-01-23 01:55:20 +0100

    Same result but better code

--- Tree.py
+++ Tree.py
@@ -8,173 +8,179 @@ MAC    = 2
 NAME   = 3
 DPID   = 4
 
+#Type of search
+INTERNAL = 0
+CHILDREN = 1
 
-"""
-" BitmapTree
-" 
-" description: 
-"""
+
+
 class BitmapTree:
+    """Bitmap Tree Class, enables ip-lookup for Ryu Openflow controler"""
+    stride = ""
+    switches_network = {}
+    root_node = ""
 
-    """
-    " __init__()
-    " 
-    " input: @self, @switch_list, @stride
-    " output:
-    " changes: @self.stride, @self.switches_network, self.root_node
-    "
-    " description: Constructor of the class, calls method to build the tree
-    """
-    def __init__(self, switch_list, stride):
+
+    def __init__(self, switch_list, stride = 2):
+        """Constructor of the class, calls method to build the tree"""
         self.stride = stride
         self.switch_list_to_switches_network(switch_list)
-        print("-- Building Bitmap Tree (stride = "+ str(self.stride) +") with "+ str(len(switch_list)) +" Switches")
+        print("-- Building Bitmap Tree (stride = "+ str(self.stride)
+            + ") with " + str(len(switch_list)) +" Switches")
         self.build_tree()
 
-    """
-    " switch_list_to_switches_network()
-    " 
-    " input: @self, @switch_list
-    " output:
-    " changes: @self.switches_network
-    "
-    " description: Converts the ips from the switch_list to binary. Then applies the network mask to the ips to get the Prefix Table.
-    """
+
     def switch_list_to_switches_network(self, switch_list):
+        """Ips and subnet to Prefix Table"""
         print("-- Creating Switches Network Array (Prefix Table)")
-        self.switches_network = {}
-        i = 0
+        switches_network = {}
         for switch in switch_list:
-            ip_network = "".join([bin(int(x)+256)[3:] for x in switch.split('.')])
-            ip_network = ip_network[:int(switch_list[switch][SUBNET])]
-            self.switches_network[ip_network] = switch
-            print("--- ["+ self.switches_network[ip_network] +"] : "+ip_network)
+            prefix = self.ip_to_prefix(switch, 
+            int(switch_list[switch][SUBNET]))
+            self.add_prefix_to_table(prefix, switch_list[switch][DPID])
         print("-- Finished Switches Network Array\n")
 
-    """
-    " switch_list_to_switches_network()
-    " 
-    " input: @self
-    " output:
-    " changes: @self.root_node
-    "
-    " description: Creates the bitmap tree. Starts by creating the root_node and then enters a recursive algorithm to build the complete tree
-    """
+    
+    def ip_to_prefix(self, ip, subnet):
+        "From ip to binary prefix"
+        ip_bin = self.ip_to_bin(ip)
+        return self.ip_bin_and_subnet_to_prefix(ip_bin, subnet)
+
+
+    def ip_to_bin(self, ip):
+        """Converts ip in format (x.x.x.x) to binary"""
+        return "".join([bin(int(x)+256)[3:] for x in ip.split('.')])
+
+
+    def ip_bin_and_subnet_to_prefix(self, ip, subnet):
+        """Cuts the IP to a prefix according to a subnet"""
+        return ip[:int(subnet)]
+
+
+    def add_prefix_to_table(self, prefix, dpid):
+        self.switches_network[prefix] = dpid
+        print("--- ["+ self.switches_network[prefix] +"] : "
+                + prefix)
+
+
     def build_tree(self):
+        """Creates the bitmap tree. 
+
+        Starts by creating the root_node and then enters a recursive 
+        algorithm to build the complete tree
+
+        """
         print("--- Creating Root Node")
         self.root_node = RootNode()
-        self.create_node(self.root_node,"")
+        self.create_node(self.root_node)
         print("-- Bitmap Tree Built with Success")
 
 
-    """
-    " create_node()
-    " 
-    " input: @self, @node, @ip
-    " output:
-    " changes: @self.root_node
-    "
-    " description: Creates the node, while creating children the method becomes recursive
-    "   Before checking for internal results and children the idxs are set to the next position availabel in each array
-    """
-    def create_node(self, node, ip):
+    def create_node(self, node, ip = ""):
+        """Creates the node, while creating children the method becomes 
+        recursive.
+        
+        Before checking for internal results and children the idxs are 
+        set to the next position availabel in each array
+        """
         print("--- Building Node")
         node.update_idx(self.root_node)
         self.check_for_internal_results(node, ip)
         self.check_for_children(node, ip)
 
-    """
-    " check_for_internal_results()
-    " 
-    " input: @self, @node, @ip
-    " output:
-    " changes: @self.root_node
-    "
-    " description: Looks in the @self.switches_network for a match with the ips represented by the @node.
-    "   If there is a match it means that the this node is representing that prefix and so it is added to the internal bitmap of that node and to the list of results.
-    """
+
     def check_for_internal_results(self, node, ip):
+        """Looks in the @self.switches_network for a match with the ips 
+        represented by the @node.
+
+        If there is a match it means that this node is representing that 
+        prefix. So this prefix result is added to the internal bitmap 
+        of that node and to the list of results.
+        """
         print("---- Building Internal Bitmap")
-        max_ip = bin(int(pow(2, self.stride))-1)[2:]
-        actual_idx = 0
         actual_ip = ""
-        while 1:
-            complete_ip = ip+actual_ip
+        for actual_idx in range(0, Node.max_nodes_in_node(self.stride, 
+            INTERNAL)):
+            complete_ip = ip + actual_ip
             if (complete_ip in self.switches_network):
-                print("----- Adding Result")
-                result = self.switches_network[ip+actual_ip]
-                del self.switches_network[ip+actual_ip]
-                print("------ ["+ str(actual_idx) +"] : "+ result)
-                node.add_result(actual_idx, result, self.root_node)
-            if actual_ip == max_ip:
-                break
-            else:
-                actual_ip = self.next_ip(actual_ip)
-                actual_idx +=1
+                self.add_result(actual_idx, complete_ip, node)
+            actual_ip = self.next_ip(actual_ip)
         print("---- Finish Internal Bitmap")
 
-    #WARNING: NOT SAFE FOR STRIDES != 2
-    """
-    " check_for_children()
-    " 
-    " input: @self, @node, @ip
-    " output:
-    " changes: @self.root_node
-    "
-    " description: Goes trough all the ips possible for children of this node and calls a method to check if there is in fact a child for that ip, creating an object if there is.
-    "   Loops the list of children created and creates the node(fills the object previously created) for each one (in this fase the function becames recursive - Each child will create all it's children before returning )
-    """
+
+    def add_result(self, idx, ip, node):
+        """Adds a result to the node"""
+        print("----- Adding Result")
+        result = self.retrieve_and_delete_result_from_switches_network(ip)
+        node.add_result(idx, result, self.root_node)
+        print("------ ["+ str(idx) +"] : "+ result)
+    
+
+    def retrieve_and_delete_result_from_switches_network(self, ip):
+        """retrives result from the switches_network list and deletes 
+        the it from the same list
+        """
+        result = self.switches_network[ip]
+        del self.switches_network[ip]
+        return result
+
+
     def check_for_children(self, node, ip):
+        """Goes trough all the ips possible for children of this node 
+        and calls a method to check if there is in fact a child for 
+        that ip, creating an object if there is.
+
+        Loops the list of children created and creates the node(fills 
+        the object previously created) for each one (in this fase the 
+        function becames recursive - Each child will create all it's 
+        children before returning )
+        """
         print("---- Building Child Bitmap")
-        i = "000"
-        position = 0
+        i = self.zero_in_binary()
         children_nodes = []
         children_ip = {}
         child_node = None
-        while True:
+        for position in range(0, Node.max_nodes_in_node(self.stride, 
+            CHILDREN)):
             complete_ip = ip + i
             child_node = self.check_for_child(node,complete_ip, position)
             if child_node:
                 children_nodes.append(child_node)
                 children_ip[children_nodes[-1]] = complete_ip
                 child_node = None
-            if i == "111":
-                break
-            i= self.next_ip(i)
-            position +=1
+            i = self.next_ip(i)
         print("---- Finish Child Bitmap")
         for child_node in children_nodes:
             self.create_node(child_node, children_ip[child_node])
 
-    """
-    " check_for_child()
-    " 
-    " input: @self, @node, @ip, @position
-    " output:
-    " changes: @self.root_node
-    "
-    " description: Looks in the @self.switches_network for a partial match with the ips represented by the @node.
-    "   If there is a partial match it means that this node is a father of the node representing that prefix. A node is created and the algorithm continues in the new node (recursive)
-    """
+
+    def zero_in_binary(self):
+        """Return, depending on the stride, the correct binary for 0"""
+        return "".join(["0" for x in range(0, self.stride +1)])
+
+
     def check_for_child(self,node,ip, position):
+        """Looks in the @self.switches_network for a partial match with 
+        the ips represented by the @node.
+        
+        If there is a partial match it means that this node is a father 
+        of the node representing that prefix. A node is created and the 
+        algorithm continues in the new node (recursive)
+        """
         for network in self.switches_network:
             if network.startswith(ip):
                 print("----- Adding Child")
                 print("------ ["+ str(position) +"] : "+ ip)
-                child_node = node.add_child (position, self.root_node)
-                return child_node
-        
-    """
-    " next_ip()
-    " 
-    " input: @self, @actual_ip
-    " output: @next_ip
-    " changes:
-    "
-    " description: Small method to get the next ip to go to trough the tree in the bitmap style (like a snake).
-    "   If there is no change in the number of bits represented it works as a simple sum of 1.
-    """          
+                return node.add_child (position, self.root_node)
+
+
     def next_ip(self, actual_ip):
+        """Small method to get the next ip to go to trough the tree in 
+        the bitmap style (like a snake). ->Used for internal search
+        
+        If there is no change in the number of bits represented it 
+        works as a simple sum of 1. -> Used for children search
+        """
         if (actual_ip == ""):
             return "0"
         last_idx = len(actual_ip) - 1
@@ -187,16 +193,9 @@ class BitmapTree:
                     return actual_ip[:i] + "1" + "".join("0" for x in range (i, last_idx))
             return "".join("0" for x in range (last_idx + 2))
 
-    """
-    " print_tree()
-    " 
-    " input: @self
-    " output: 
-    " changes:
-    "
-    " description: Prints the tree
-    """    
+
     def print_tree(self):
+        """Prints the tree """
         print("\n\nPRINTING TREE\n--ROOT NODE")
         self.root_node.print_node()
         i=0
@@ -224,11 +223,11 @@ switch_list["0.0.0.254"  ] = ["0.0.0.254","0" ,"00:00:00:11:11:01","s1","1"]
 switch_list["128.128.0.254"] = ["128.128.0.254","1","00:00:00:11:11:02","s2","2"] 
 switch_list["0.128.0.254"] = ["0.128.0.254","2","00:00:00:11:11:03","s3","3"] 
 switch_list["160.160.0.254"] = ["160.160.0.254","3","00:00:00:11:11:04","s4","4"]
-switch_list["224.0.0.254"  ] = ["224.0.0.254","3" ,"00:00:00:11:11:01","s1","1"] 
-switch_list["128.128.0.253"] = ["128.128.0.254","4","00:00:00:11:11:02","s2","2"] 
-switch_list["232.128.0.254"] = ["232.128.0.254","5","00:00:00:11:11:03","s3","3"] 
-switch_list["228.160.0.254"] = ["228.160.0.254","6","00:00:00:11:11:04","s4","4"]
-switch_list["134.160.0.254"] = ["134.160.0.254","7","00:00:00:11:11:04","s4","4"]
+switch_list["224.0.0.254"  ] = ["224.0.0.254","3" ,"00:00:00:11:11:01","s5","5"] 
+switch_list["128.128.0.253"] = ["128.128.0.254","4","00:00:00:11:11:02","s6","6"] 
+switch_list["232.128.0.254"] = ["232.128.0.254","5","00:00:00:11:11:03","s7","7"] 
+switch_list["228.160.0.254"] = ["228.160.0.254","6","00:00:00:11:11:04","s8","8"]
+switch_list["134.160.0.254"] = ["134.160.0.254","7","00:00:00:11:11:04","s9","9"]
 
 
 tree = BitmapTree(switch_list, 2)
--- node.py
+++ node.py
@@ -1,120 +1,92 @@
-#NODE CLASS
-INTERNAL    = 100
-CHILD       = 101
-COMPLETE    = 102
-MAX_ITERNAL_BITMAP = 7
-MAX_CHILD_BITMAP = 8
+#Type of search
+INTERNAL = 0
+CHILDREN = 1
 
-"""
-" Node
-" 
-" description: class for a node in a bitmap binary tree
-"""
+
 class Node:
-    """
-    " __init__()
-    " 
-    " input: @self
-    " output:
-    " changes: @self.internal_idx, @self.child_idx, @self.internal_bitmap, @self.child_bitmap
-    "
-    " description: Constructor of the class, initializes the variables
-    """
-    def __init__(self):
-        #Initialized for root_node - For other nodes it is updated during children discovery:
-        self.internal_idx = 0
-        self.child_idx = 0
-        self.initialize_bitmap()
+    """Class for a node in a bitmap binary tree """
+    internal_idx = ""
+    child_idx = ""
+    internal_bitmap = []
+    child_bitmap = []
 
-    #WARNING: NOT SAFE FOR STRIDES != 2
-    """
-    " initialize_bitmap()
-    " 
-    " input: @self
-    " output:
-    " changes: @self.internal_bitmap, @self.child_bitmap
-    "
-    " description: Initializes bitmaps to zero
-    """
-    def initialize_bitmap(self):
-        self.internal_bitmap = [0,0,0,0,0,0,0]
-        self.child_bitmap = [0,0,0,0,0,0,0,0]
 
-    """
-    " add_result()
-    " 
-    " input: @self, @position, @result
-    " output:
-    " changes: @self.internal_bitmap, @root_node
-    "
-    " description: Changes to 1 the @position of the @internal_bitmap and calls the method @add_result_info of @root_node to keep the @result
-    """
+    def __init__(self, internal_idx = 0, child_idx = 0, stride = 2):
+        """Constructor of the class, initializes the variables
+
+        Initialized for root_node - For other nodes it is updated 
+        during children discovery
+        """
+        self.internal_idx = internal_idx
+        self.child_idx = child_idx
+        self.initialize_bitmap(stride)
+
+
+    def initialize_bitmap(self, stride = 2):
+        """Initializes bitmaps to zero"""
+        self.internal_bitmap = []
+        self.child_bitmap = []
+        for i in range(0,self.max_nodes_in_node(stride)):
+            self.internal_bitmap.append(0)
+            self.child_bitmap.append(0)
+        self.child_bitmap.append(0)
+
+
+    @staticmethod
+    def max_nodes_in_node(stride, type_of_search = INTERNAL):
+        """Defines max number of binary trie nodes inside a bitmapTree
+
+        Can also be used to know the number of binary trie nodes that 
+        are children of a bitmapTree node
+        """
+        if type_of_search is INTERNAL:
+            return int(pow(2, stride +1) - 1)
+        else:
+            return int(pow(2, stride +1))
+
+
     def add_result(self, position, result, root_node):
+        """Changes to 1 the @position of the @internal_bitmap and calls 
+        the method @add_result_info of @root_node to keep the @result
+        """
         self.internal_bitmap[position] = 1
         root_node.add_result_info(result)
 
-    
-    """
-    " update_idx()
-    " 
-    " input: @self, @root_node
-    " output: 
-    " changes: @self.child_idx, @self.internal_idx
-    "
-    " description: Updates the idxs so they match the next position in the respective arrays
-    """
+
     def update_idx(self, root_node):
+        """Updates the idxs so they match the next position in the 
+        respective arrays"""
         self.child_idx = len(root_node.children)
         self.internal_idx = len(root_node.results)
 
-    """
-    " add_child()
-    " 
-    " input: @self, @position, @root_node
-    " output: @child_node
-    " changes: @self.internal_bitmap
-    "
-    " description: Changes to 1 the @position of the @child_bitmap and calls the method @add_child_node of @root_node to create a new @child_node
-    """
+
     def add_child(self, position, root_node):
+        """Changes to 1 the @position of the @child_bitmap and calls 
+        the method @add_child_node of @root_node to create a new 
+        @child_node
+        """
         self.child_bitmap[position] = 1
         return root_node.add_child_node()
     
-    """
-    " number_of_ones()
-    " 
-    " input: @self, @bitmap_id, @position
-    " output: @number_of_ones
-    " changes: 
-    "
-    " description: Counts the @number_of_ones before @position in the bitmap indicated by @bitmap_id 
-    """
+
     def number_of_ones(self, bitmap_id, position):
+        """Counts the @number_of_ones before @position in the bitmap 
+        indicated by @bitmap_id 
+        """
         number_of_ones = 0
         if (bitmap_id == INTERNAL):
-            if (position == COMPLETE):
-                position = MAX_INTERNAL_BITMAP
             for i in range(0, position - 1):
                 if (self.internal_bitmap[i] == 1):
                     number_of_ones +=1
         elif (bitmap_id == CHILD):
-            if (position == COMPLETE):
-                position = MAX_CHILD_BITMAP
             for i in range(0, position - 1):
                 if (self.child_bitmap[i] == 1):
                     number_of_ones +=1
         return number_of_ones
 
-    """
-    " print_node()
-    " 
-    " input: @self
-    " output: 
-    " changes: 
-    "
-    " description: Prints the node: The idxs and the bitmaps
-    """
+
     def print_node (self):
+        """Prints the node: The idxs and the bitmaps"""
         print ("["+str(self.internal_idx)+"]", end=" ")
         for i in self.internal_bitmap:
             print (str(i)+"|", end="")
@@ -125,48 +97,28 @@ class Node:
         print ("")
         
 
-"""
-" RootNode(Node)
-" 
-" description: class for the root_node in a bitmap binary tree (extends class Node)
-"""
 class RootNode(Node):
-    """
-    " __init__()
-    " 
-    " input: @self
-    " output:
-    " changes: @self.internal_idx, @self.child_idx, @self.internal_bitmap, @self.child_bitmap
-    "
-    " description: Constructor of the class, initializes the variables. As this is the root_node the Super Constructor is called with idxs = 0
-    """
-    def __init__(self):
-        super(RootNode, self).__init__()
-        self.results = []
-        self.children = []
+    """class for the root_node in a bitmap binary tree (extends Node)"""
+    results = []
+    children = []
 
-    """
-    " add_result_info()
-    " 
-    " input: @self, @result
-    " output: 
-    " changes: @self.results
-    "
-    " description: Adds a result to the @self.results array
-    """
+
+    def __init__(self):
+        """Constructor of the class, initializes the variables"""
+        super(RootNode, self).__init__()
+        results = []
+        children = []
+
+
     def add_result_info(self, result):
+        """Adds a result to the @self.results array"""
         self.results.append(result)
 
-    """
-    " add_child_node()
-    " 
-    " input: @self
-    " output: @child_node
-    " changes: @self.children
-    "
-    " description: Creates a new node, with the current array sizes as idxs and puts it in the @self.children array
-    """
+
     def add_child_node(self):
+        """Creates a new node, with the current array sizes as idxs and 
+        puts it in the @self.children array
+        """
         child = Node()
         self.children.append(child)
         return child
