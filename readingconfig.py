a7ca39a35c9be2b52045884611d821092b5d2677
franciscof93 <franciscofsantos11@gmail.com>
2016-02-18 00:51:38 +0100

    Readingconfig3

--- readingconfig.py
+++ readingconfig.py
@@ -13,9 +13,13 @@ COUNT = 1
 
 class ReadingFromFile:
 
-    def __init__(self):
-        print ("aqui")
-        self.read_file('config')
+    switch_ip = {}
+    mac_to_port = {}
+    switch = {}
+    ip_to_mac = {}
+
+    def __init__(self, filename):
+        self.read_file(filename)
 
     @staticmethod
     def split_line(line, mode):
@@ -29,8 +33,11 @@ class ReadingFromFile:
         line_s = line.split(' ')
 
         if (mode ==INFOIP):
-            d = [line_s[0],line_s[3].replace('\n', '')]
+            d = [line_s[0],line_s[3].replace('\n', ''),'']
             d[0]=d[0][1:]
+            val_s = d[1].split('/')
+            d[1]=val_s[0]
+            d[2]=val_s[1]
         elif (mode==INFOGTAWAY):
             d = [line_s[0],line_s[5].replace('\n', '')]
             d[0]=d[0][1:]
@@ -48,24 +55,19 @@ class ReadingFromFile:
         state = NOINFO
         buffer = {}
         counter = 0
-        switch_ip = {}
-        mac_to_port = {}
-        switch = {}
-        ip_to_mac = {}
         count =1
 
-        fr=open('config', 'r')
+        fr=open(filename, 'r')
 
         for line in fr:
             if (line != "\n"):
                 state = self.split_line(line,NOINFO)
 
                 if (state == "ifconfig"):
-                    a=self.split_line(line,INFOIP)
-                    val_s = a[1].split('/')
-                    buffer[a[0]]= {}
-                    buffer[a[0]][PREFIX] = [val_s[0]]
-                    buffer[a[0]][MASK] = [val_s[1]]	
+                    line_splitted=self.split_line(line,INFOIP)
+                    buffer[line_splitted[0]]= {}
+                    buffer[line_splitted[0]][PREFIX] = line_splitted[1]
+                    buffer[line_splitted[0]][MASK] = line_splitted[2]	
 				
                 elif (state == "route"):
                     a=self.split_line(line,INFOGTAWAY)
@@ -80,15 +82,15 @@ class ReadingFromFile:
 
             numb = str(count)
             counter_mac=counter_mac+1
-            ip_to_mac[buffer[key][PREFIX][0]]=["00:00:00:00:00:"+str(format(counter_mac,'02x'))]
+            self.ip_to_mac[buffer[key][PREFIX]]="00:00:00:00:00:"+str(format(counter_mac,'02x'))
 
 
             if not (any(buffer[key][GTWY] in s for s in visited)):
-                switch[numb] = [buffer[key][GTWY], buffer[key][MASK],"00:00:00:11:11:"+str(format(counter_mac,'02x')) , "s"+numb, numb]
-                switch_ip[buffer[key][GTWY]]=[str(count)]
+                self.switch[numb] = [buffer[key][GTWY], buffer[key][MASK],"00:00:00:11:11:"+str(format(counter_mac,'02x')) , "s"+numb, numb]
+                self.switch_ip[buffer[key][GTWY]]=[str(count)]
 
-                mac_to_port[numb]={}
-                mac_to_port[numb]["00:00:00:00:00:"+str(format(counter_mac,'02x'))]= 1
+                self.mac_to_port[numb]={}
+                self.mac_to_port[numb]["00:00:00:00:00:"+str(format(counter_mac,'02x'))]= 1
 
                 count_mtp[buffer[key][GTWY]] = {}
                 count_mtp[buffer[key][GTWY]][NUMB] = numb
@@ -99,23 +101,20 @@ class ReadingFromFile:
 				
             else:
                 count_mtp[buffer[key][GTWY]][COUNT] += 1
-                mac_to_port[count_mtp[buffer[key][GTWY]][NUMB]]["00:00:00:00:00:"+str(format(counter_mac,'02x'))]= count_mtp[buffer[key][GTWY]][COUNT]
+                self.mac_to_port[count_mtp[buffer[key][GTWY]][NUMB]]["00:00:00:00:00:"+str(format(counter_mac,'02x'))]= count_mtp[buffer[key][GTWY]][COUNT]
                 
         print("zzzzzzzzzzzzzzzzzz")
-        print (mac_to_port)
-        self.mac_to_port = mac_to_port
+        print (self.mac_to_port)
         print("zzzzzzzzzzzzzzzzzz")
-        print (switch)
-        self.switch = switch
+        print (self.switch)
         print("zzzzzzzzzzzzzzzzzz")
-        print (switch_ip)
-        self.switch_ip = switch_ip
+        print (self.switch_ip)
         print("zzzzzzzzzzzzzzzzzz")
-        print (ip_to_mac)
-        self.ip_to_mac = ip_to_mac
+        print (self.ip_to_mac)
         print ("asfasdfsdf")      
-     	#fr.close()
 
-a = ReadingFromFile()
+        fr.close()
+
+a = ReadingFromFile('config')
     
             
